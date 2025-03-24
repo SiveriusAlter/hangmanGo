0d982d8bf0f71df6df1b01f7cb02a132ee6ebeef
Alexander Zhernakov <neongenesisdatabase@gmail.com>
2025-03-25 00:23:40 +0500

    Added letters input

---
+++ hangman.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+	"unicode/utf8"
+)
+
+func main() {
+	letter := getLetter()
+	fmt.Println(letter)
+}
+
+func getLetter() string {
+	var letter string
+	for {
+		fmt.Println("Введите одну букву на русском языке: ")
+		fmt.Scan(&letter)
+		if utf8.RuneCountInString(letter) == 1 && checkString(letter) {
+			return strings.ToLower(letter)
+		}
+	}
+}
+
+func checkString(str string) bool {
+	re, _ := regexp.Compile("[А-Яа-яЁё]")
+	return re.MatchString(str)
+}
